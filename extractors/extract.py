dc2c6572891aca2a98c38d653096490885845263
Lucas Neo <lucasneosr@gmail.com>
2016-06-18 10:08:10 +0800

    methods for parsing zendesk response into a pandas dataframe

--- extractors/extract.py
+++ extractors/extract.py
@@ -120,7 +120,7 @@ class FromMixpanelRaw:
 
         Args:
             filename (string): The filename containing the newline JSON.
-            events: The
+            events: The number of events to be parsed.
         """
         # create a df for storing the results
         result = pd.DataFrame()
@@ -180,5 +180,102 @@ class FromMixpanelRaw:
 
         # convert time column from epoch to utc
         result['time'] = pd.to_datetime(result.time, unit='s')
+
+        return result
+
+class FromZendeskTickets:
+    """
+    Runs the incremental exporter to export Zendesk tickets.
+    Args:
+        start_time (string): Datetime period to extract data for.
+
+    Attributes:
+        ticket_fields (tuple): Fields that should be extracted from the results.
+    """
+    def __init__(self, start_time=None):
+        self.start_time = start_time
+
+        # list of fields to parse
+        self.ticket_fields = (
+            'assignee_id','created_at','description','due_at','external_id','generated_timestamp',
+            'id','organization_id','priority','subject','raw_subject','recipient','satisfaction_rating',
+            'priority','requester_id','submitter_id','updated_at','status','tags'
+        )
+
+    def RunAPI(self, tickets=0, to_csv=False, filename=None):
+        """
+        gets 1 page of data from Zendesk as a pandas dataframe.
+        """
+        # retrieve the response
+        response = json.loads(api.Zendesk().request_incremental(self.start_time))
+        result = self.ParseTickets(response['tickets'], number=tickets)
+
+        # option to write data to csv
+        if to_csv == True:
+            if filename is None:
+                raise ValueError('CSV filename cannot be blank.')
+            else:
+                result.to_csv(filename, encoding='utf-8')
+                return result
+
+        return result
+
+    def ParseTickets(self, tickets, number=0):
+        """
+        """
+        # check if tickets is a list of tickets
+        if isinstance(tickets, list) == False:
+            raise ValueError('Pass in a list of tickets.')
+
+        # check if number is an int
+        number = int(number)
+
+        result = pd.DataFrame()
+
+        # iterate over the tickets
+        for index, ticket in enumerate(tickets):
+            if (number > 0 and index > number):
+                break
+
+            # select the initial fields from each ticket
+            data = {k: ticket.get(k, None) for k in self.ticket_fields}
+
+            # flatten the dictionary
+            data = self.FlattenDict(data)
+
+            # concatenate lists into strings
+            for key, value in data.items():
+                if isinstance(value, list):
+                    data[key] = ','.join(value)
+
+            # convert the dictionary into a dataframe and merge it with the result
+            data = pd.DataFrame(data, index=[index])
+            result = pd.concat((result, data))
+
+        return result
+
+    def FlattenDict(self, dictionary, parent_key='', result=None):
+        """
+        Recursively flattens nested dictionaries by concatenating the keys together.
+
+        Args:
+            dictionary (dict): Dictionary to be unnested.
+            parent_key (string): The initial string which accumulates the keys.
+            result (dict): The accumulator for the recursive function.
+
+        Returns:
+            result (dictionary): An unnested dictionary with concatenated keys.
+        """
+        # initialise the dictionary accumulator
+        result = {} if result is None else result
+
+        # loop over the keys and recursively concatenate them
+        for key, value in dictionary.items():
+            new_key = key if parent_key == '' else '|'.join([parent_key, key])
+
+            if isinstance(value, dict):
+                self.FlattenDict(value, parent_key=new_key, result=result)
+            else:
+                result[new_key] = value
 
         return result
